d9517c4366169d207256c6c77697148b09345220
Colby Williams <colbylwilliams@gmail.com>
2016-09-02 14:57:44 -0400

    commit ApiDefinitions and fixed bitcode issues

---
+++ extern/extras/StructsAndEnums.cs
@@ -0,0 +1,13 @@
+using System;
+using ObjCRuntime;
+
+namespace Xamarin.TTTAttributedLabel
+{
+	[Native]
+	public enum TTTAttributedLabelVerticalAlignment : ulong
+	{
+		Center = 0,
+		Top = 1,
+		Bottom = 2
+	}
+}
